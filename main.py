2c837b8930c726b53f8121f5e8c15936c9b63971
Longcat2957 <schrodingers030@gmail.com>
2022-07-07 11:45:48 +0900

    minor change

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     frames_per_clip=16,
     train=True,
     step_between_clips=4,
-    num_workers=8,
+    num_workers=48,
     transform=VideoTransform(resize=(224, 224), phase='train')
     )
 
@@ -25,18 +25,18 @@ if __name__ == '__main__':
     frames_per_clip=16,
     step_between_clips=4,
     train=False,
-    num_workers=8,
+    num_workers=48,
     transform=VideoTransform(resize=(224, 224), phase='val')
     )
 
-    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=True)
     
     mydataloadersdict = {'train' : train_dataloader, 'val' : val_dataloader}
 
     from model.eco_lite import ECO_Lite
     mymodel = ECO_Lite()
     myloss = nn.CrossEntropyLoss()
-    myoptimizer = torch.optim.SGD(mymodel.parameters(),lr=1e-5, momentum=0.9, dampening=0, nesterov=True)
+    myoptimizer = torch.optim.Adam(mymodel.parameters(), lr=0.001, betas=(0.9, 0.999))
 
     train_model(mymodel, mydataloadersdict, myloss, myoptimizer, 5)
