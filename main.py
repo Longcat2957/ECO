cb7368492d75d4e52961534019779d4e0e8224b8
Longcat2957 <schrodingers030@gmail.com>
2022-07-08 17:20:31 +0900

    train_loop

--- main.py
+++ main.py
@@ -3,11 +3,70 @@ import sys
 import numpy as np
 from dataset import make_datapath_list, VideoTransform, get_label_id_dictionary, VideoDataset
 from model.eco_lite import ECO_Lite
-from train import train_model
+
 
 import argparse
 import time
 import datetime
+
+import torch
+from tqdm import tqdm
+import time
+import copy
+
+def train_model(model, dataloaders_dict, criterion, optimizer, num_epochs):
+    since = time.time()
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs -1}')
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, _, labels, _ in tqdm(dataloaders_dict[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            
+            epoch_loss = running_loss / len(dataloaders_dict[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders_dict[phase].dataset)
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+        print()
+    
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')   
+
+    model.load_state_dict(best_model_wts)
+    return model
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -17,8 +76,8 @@ if __name__ == '__main__':
     parser.add_argument("--batch_size", type=int, default=1, help="batch_size")
     parser.add_argument("--epochs", type=int, default=10, help="number of epochs")
     parser.add_argument("--checkpoint", type=str, default=None, help = "Optional path to checkpoint model")
+    parser.add_argument("--savepath", type=str, default='weights/best.pth', help="Save path config")
     parser.add_argument("--classid", type=str, default="./classid.txt", help="class & id config")
-    parser.add_argument("--save_interval", type=int, default=4, help="save interval")
 
     opt = parser.parse_args()
     print(opt)
@@ -46,11 +105,14 @@ if __name__ == '__main__':
         'val' : val_dataloader
     }
 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     my_model = ECO_Lite(num_of_classes=opt.classes)
     if opt.checkpoint is not None:
         my_model = torch.load(opt.checkpoint)
     my_loss = torch.nn.CrossEntropyLoss()
     my_opt = torch.optim.Adam(my_model.parameters(),lr=1e-3, betas=(0.9, 0.999))
 
-    EPOCH = opt.epochs
-    train_model(my_model, whole_dataloader, my_loss, my_opt, EPOCH, opt.save_interval)
+    my_model.to(device)
+    result = train_model(my_model, whole_dataloader, my_loss, my_opt, opt.epochs)
+    torch.save(result.state_dict(), opt.savepath)
